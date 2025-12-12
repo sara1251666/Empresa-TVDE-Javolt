7014eb80b47caeef1f52be2a302a9003cbe53579
odranoeL <1251663@isep.ipp.pt>
2025-12-12 22:07:38 +0000

    correção da alteração dos comentarios

--- TVDE_Javolt/src/Viaturas.java
+++ TVDE_Javolt/src/Viaturas.java
@@ -1,32 +1,32 @@
 /**
- * Classe Veiculo
- * Representa uma viatura da empresa TVDE.
+ * Classe Viaturas
+ * Representa um carro da empresa TVDE.
+ * Guarda os dados principais do veículo.
  * @author Leonardo
  * @version 1
  * @since 12/12/2025
  */
 public class Viaturas {
 
-    // Número da matrícula do carro
+    // Matrícula do carro
     private String matricula;
 
-    // Marca do carro (ex: Toyota, BMW)
+    // Marca do carro
     private String marca;
 
-    // Modelo do carro (ex: Corolla, Série 3)
+    // Modelo do carro
     private String modelo;
 
     // Ano em que o carro foi fabricado
     private int anoFabrico;
 
     /**
-     * Construtor da classe Viaturas
-     * Serve para criar um novo carro com todos os seus dados.
-     *
+     * Construtor da classe Viaturas.
+     * Cria um carro com todos os seus dados.
      * @param matricula matrícula do carro
      * @param marca marca do carro
      * @param modelo modelo do carro
-     * @param anoFabrico ano em que o carro foi fabricado
+     * @param anoFabrico ano de fabrico do carro
      */
     public Viaturas(String matricula, String marca, String modelo, int anoFabrico) {
         this.matricula = matricula;
@@ -36,71 +36,80 @@ public class Viaturas {
     }
 
     /**
-     * Devolve a matrícula do carro
+     * Devolve a matrícula do carro.
+     * @return matrícula do carro
      */
     public String getMatricula() {
         return matricula;
     }
 
     /**
-     * Altera a matrícula do carro
+     * Altera a matrícula do carro.
+     * @param matricula nova matrícula do carro
      */
     public void setMatricula(String matricula) {
         this.matricula = matricula;
     }
 
     /**
-     * Devolve a marca do carro
+     * Devolve a marca do carro.
+     * @return marca do carro
      */
     public String getMarca() {
         return marca;
     }
 
     /**
-     * Altera a marca do carro
+     * Altera a marca do carro.
+     * @param marca nova marca do carro
      */
     public void setMarca(String marca) {
         this.marca = marca;
     }
 
     /**
-     * Devolve o modelo do carro
+     * Devolve o modelo do carro.
+     * @return modelo do carro
      */
     public String getModelo() {
         return modelo;
     }
 
     /**
-     * Altera o modelo do carro
+     * Altera o modelo do carro.
+     * @param modelo novo modelo do carro
      */
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
 
     /**
-     * Devolve o ano de fabrico do carro
+     * Devolve o ano de fabrico do carro.
+     * @return ano de fabrico do carro
      */
     public int getAnoFabrico() {
         return anoFabrico;
     }
 
     /**
-     * Altera o ano de fabrico do carro
+     * Altera o ano de fabrico do carro.
+     * @param anoFabrico novo ano de fabrico do carro
      */
     public void setAnoFabrico(int anoFabrico) {
         this.anoFabrico = anoFabrico;
     }
 
     /**
-     * Mostra todas as informações do carro em texto
+     * Converte os dados do carro para texto.
+     * @return texto com matrícula, marca, modelo e ano de fabrico
      */
     @Override
     public String toString() {
-        return "Veículo {" +
-                "Matrícula = '" + matricula + '\'' +
-                ", Marca = '" + marca + '\'' +
-                ", Modelo = '" + modelo + '\'' +
-                ", Ano de Fabrico = " + anoFabrico +
+        return "Veiculo {" +
+                "matricula='" + matricula + '\'' +
+                ", marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", anoFabrico=" + anoFabrico +
                 '}';
     }
 }
