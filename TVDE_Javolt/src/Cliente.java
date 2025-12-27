b00f1b87942502b1b757be904ceb7abd4501ba63
saralondreira1251666 <1251666@isep.ipp.pt>
2025-12-27 18:39:10 +0000

    Alteração da Classe Condutor, Cliente e Pessoa.

--- TVDE_Javolt/src/Cliente.java
+++ TVDE_Javolt/src/Cliente.java
@@ -5,12 +5,34 @@
  * @version 2
  * @since 2025-12-12
  */
-public class Cliente extends Pessoa{
+public class Cliente extends Pessoa {
 
+    /**
+     * Construtor Cliente Vazio.
+     */
+    public Cliente() {
+    }
+
+    /**
+     * Constrói um Novo Cliente com os Dados Fornecidos.
+     * Invoca o Construtor da Supercclasse (Pessoa).
+     *
+     * @param nome      Nome do Cliente.
+     * @param nif       Número de Identificação Fiscal.
+     * @param tel       Número de Telemóvel.
+     * @param morada    Morada Completa.
+     * @param cartaoCid Número do Cartão do Cidadão.
+     */
     public Cliente(String nome, int nif, int tel, String morada, int cartaoCid) {
         super(nome, nif, tel, morada, cartaoCid);
     }
 
+    /**
+     * Devolve a Representação em Texto do Cliente.
+     * Inclui a etiqueta "Cliente" e os dados herdados de Pessoa.
+     * @return String Formatada com os dados do cliente.
+     */
+    @Override
     public String toString() {
         return "Cliente [" + super.toString() + "]";
     }
--- TVDE_Javolt/src/Condutor.java
+++ TVDE_Javolt/src/Condutor.java
@@ -1,154 +1,79 @@
+import java.text.ParsePosition;
+
 /**
  * Representa um Condutor
  * @author Levi e Sara
  * @version 1
  * @since 2025-12-12
  */
-public class Condutor {
-    private int cartaoCid;  // Número de Cartão de Cidadão
-    private String nome;    // Nome do Cliente
-    private String cartaCond;  // Número de Cartão de Condução
-    private int nif;        // Nif do CLiente
-    private int segSocial;  // Número de Segurança Social
-    private int tel;        // Número do Telemóvel
-    private String morada;  // Morada do Cliente
+public class Condutor extends Pessoa {
+    private String cartaCond;
+    private int segSocial;
 
     /**
-     * Construtor Vazio
+     * Construtor Condutor Vazio.
      */
     public Condutor() {
     }
 
     /**
-     *
-     * @param cartaoCid Dado de Cartão de Cidadão (identificador único)
-     * @param nome Nome do Condutor
-     * @param cartaCond Dado do Número da Carta de Condução do Condutor
-     * @param nif Dado do NIF (Contribuinte) do Condutor
-     * @param segSocial Dado do Número da Segurança Social do Condutor
-     * @param tel Número de Telemóvel do Condutor
-     * @param morada Morada do Condutor
+     * Constroi um novo Condutor com os dados fornecidos.
+     * Invoca o Construtor da Superclasse (Pessoa).
+     * @param nome Nome do Condutor.
+     * @param nif Nif do Condutor.
+     * @param tel Telemóvel do Condutor.
+     * @param morada Morada do Condutor.
+     * @param cartaoCid Cartão de Cidadão do Condutor.
+     * @param cartaCond Carta de Condução do Condutor.
+     * @param segSocial Número de Segurança Social do Condutor.
      */
-    public Condutor(int cartaoCid, String nome, String cartaCond, int nif, int segSocial, int tel, String morada) {
-        this.cartaoCid = cartaoCid;
-        this.nome = nome;
+    public Condutor(String nome, int nif, int tel, String morada, int cartaoCid, String cartaCond, int segSocial) {
+        super(nome, nif, tel, morada, cartaoCid);
         this.cartaCond = cartaCond;
-        this.nif = nif;
         this.segSocial = segSocial;
-        this.tel = tel;
-        this.morada = morada;
     }
 
     /**
-     * Obtém o Número do Cartão de Cidadão
-     * @return Retorna o número do Cartão de Cidadão
-     */
-    public int getCartaocid() {
-        return cartaoCid;
-    }
-
-    /**
-     * Define um Novo Número de Cartão de Cidadão
-     * @param cartaoCid Atribui o novo valor ao Cartão de Cidadão
-     */
-    public void setCartaocid(int cartaoCid) {
-        this.cartaoCid = cartaoCid;
-    }
-
-    /**
-     * Obtém o Nome do Condutor
-     * @return Retorna o Nome do Condutor
-     */
-    public String getNome() {
-        return nome;
-    }
-
-    /**
-     * Define um Novo Nome ao CLiente
-     * @param nome Atribui o novo nome ao Cliente
-     */
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    /**
-     * Obtém o Número da Carta de Condução
-     * @return Retorna o número da Carta de Condução
+     * Obtém a Carta de Condução
+     * @return Retorna a Carta de Condução.
      */
     public String getCartaCond() {
         return cartaCond;
     }
 
     /**
-     * Define um Novo Número da Carta de Condução
-     * @param cartaCond Atribui o novo valor à Carta de Condução
+     * Define um Novo valor para a Carta de Condução.
+     * @param cartaCond Atribui a Nova Carta de Condução.
      */
-    public void setCartacond(String cartaCond) {
+    public void setCartaCond(String cartaCond) {
         this.cartaCond = cartaCond;
     }
 
     /**
-     * Obtém o Nif (Contribuinte) do Condutor
-     * @return Retorna o Nif (Contribuinte) do Condutor
-     */
-    public int getNif() {
-        return nif;
-    }
-
-    /**
-     * Define um Novo NIF ao Condutor
-     * @param nif Atribui o novo NIF (Contribuinte) ao Condutor
-     */
-    public void setNif(int nif) {
-        this.nif = nif;
-    }
-
-    /**
-     * Obtém o Número de Segurança Social do Condutor
-     * @return Retorna o Número de Segurança Social do Condutor
+     * Obtém o Número da Segurança Social.
+     * @return Retorna o Número da Segurança Social.
      */
     public int getSegSocial() {
         return segSocial;
     }
 
     /**
-     * Define um Novo Número de Segurança Social ao Condutor
-     * @param segSocial Atribui o novo Número de Segurança Social ao Condutor
+     * Define um Novo Número da Segurança Social.
+      * @param segSocial Atribui o Novo Número da Segurança Social.
      */
     public void setSegSocial(int segSocial) {
         this.segSocial = segSocial;
     }
 
     /**
-     * Obtém o Número do Telemóvel do Condutor
-     * @return Retorna o Número de Telemóvel do Condutor
+     * Devolve a Representação em Texto do Condutor
+     * Junta os dados de PEssoa com os dados especifícos do condutor.
+     * @return String com os Dados do Condutor.
      */
-    public int getTel() {
-        return tel;
-    }
-
-    /**
-     * Define um Novo Número de Telemóvel ao Condutor
-     * @param tel Atribui o novo Número de Telemóvel ao Condutor
-     */
-    public void setTel(int tel) {
-        this.tel = tel;
-    }
-
-    /**
-     * Obtém a Morada do Condutor
-     * @return Retorna a Morada do Condutor
-     */
-    public String getMorada() {
-        return morada;
-    }
-
-    /**
-     * Define uma Nova Morada ao Condutor
-     * @param morada Atribui uma Nova Morada ao Condutor
-     */
-    public void setMorada(String morada) {
-        this.morada = morada;
+    @Override
+    public String toString() {
+        return "Condutor [" + super.toString() + " | Carta: " +cartaCond + " | SegSocial: " + segSocial + "]";
     }
 }
+
 
